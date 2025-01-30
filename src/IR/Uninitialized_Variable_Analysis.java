dc419ba73e5d73b28494e617a9832d37d99c94f3
Liron Tzadok <liront@mail.tau.ac.il>
2025-01-30 10:09:34 +0200

    update kill and gen

--- src/IR/Uninitialized_Variable_Analysis.java
+++ src/IR/Uninitialized_Variable_Analysis.java
@@ -26,10 +26,19 @@ public class Uninitialized_Variable_Analysis extends Analysis {
             // might want to modify based on scope
             IRcommand_Store storeCommand = (IRcommand_Store) command;
             String varName = storeCommand.var_name;
-            for (int i=0; i<cfg.size(); i++){
+            for (int i=0; i<cfg.size(); i++) {
                 killSet.add(new Dom(varName, i));
             }
             killSet.add(new Dom(varName, null));
+        }
+        if (command instanceof IRcommand_Load){
+            // might want to modify based on scope
+            IRcommand_Load loadCommand = (IRcommand_Load) command;
+            String tempName = loadCommand.dst;
+            for (int i=0; i<cfg.size(); i++) {
+                killSet.add(new Dom(tempName, i));
+            }
+            killSet.add(new Dom(tempName, null));
         }
         return killSet;
     }
@@ -38,11 +47,51 @@ public class Uninitialized_Variable_Analysis extends Analysis {
     public HashSet<Dom> gen(CFG_Node node){
         HashSet<Dom> genSet = new HashSet<>();
         IRcommand command = node.getCommand();
-        if (command instanceof IRcommand_Store){
+        HashSet<Dom> inSet = node.getAnalysisIn();
+
+        if(command instanceof IRcommand_Binop_Sub_Integers 
+            || command instanceof IRcommand_Binop_Add_Integers
+            || command instanceof IRcommand_Binop_Div_Integers
+            || command instanceof IRcommand_Binop_Mul_Integers
+            || command instanceof IRcommand_Binop_EQ_Integer
+            || command instanceof IRcommand_Binop_GT_Integers
+            || command instanceof IRcommand_Binop_LT_Integers
+        ) {
+            // might not work without casting, if so, we would need to seperate this if to 7 different if :(
+            if(this.isLabelNull(inSet, "t" + command.t1.serial) || if(this.isLabelNull(inSet, "t" + command.t2.serial))) {
+                genSet.add(new Dom("t" + command.dst.serial, null));
+            } else {
+                genSet.add(new Dom("t" + command.dst.serial, node.getIndex()));
+            }
+        }
+
+        if (command instanceof IRcommandConstInt) {
+            IRcommandConstInt constIntCommand = (IRcommandConstInt) command;
+            genSet.add(new Dom("t" + constIntCommand.t.serial, node.getIndex()));
+        }
+        
+        if (command instanceof IRcommand_load) {
+            IRcommand_load loadCommand = (IRcommand_load) command;
+            // might want to modify based on scope
+            if(this.isLabelNull(inSet, loadCommand.var_name)) {
+                genSet.add(new Dom("t" + loadCommand.dst.serial, null));
+            }
+            else {
+                genSet.add(new Dom("t" + loadCommand.dst.serial, node.getIndex()));
+            }
+        }
+
+        if (command instanceof IRcommand_Store) {
             IRcommand_Store storeCommand = (IRcommand_Store) command;
             // might want to modify based on scope
-            genSet.add(new Dom(storeCommand.var_name, node.getIndex()));
+            if(this.isLabelNull(inSet, "t" + storeCommand.src.serial)) {
+                genSet.add(new Dom(storeCommand.var_name, null));
+            }
+            else {
+                genSet.add(new Dom(storeCommand.var_name, node.getIndex()));
+            }
         }
+
         return genSet;
     }
     
@@ -96,5 +145,13 @@ public class Uninitialized_Variable_Analysis extends Analysis {
         }
 
         initialValue = initVal;
+    }
+
+    public boolean isLabelNull(HashSet<Dom> inSet, String name) {
+        return inSet.stream()
+                .filter(dom -> name.equals(dom.getVarName()))
+                .findFirst()
+                .map(dom -> dom.getLabel() == null)
+                .orElse(false);
     }
 }
